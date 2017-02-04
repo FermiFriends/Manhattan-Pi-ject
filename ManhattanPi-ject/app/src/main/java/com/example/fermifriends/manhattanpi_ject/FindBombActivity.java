6e63eba3ea75d835ac6a184f882f15235cf0393a
Daniel Dean <dd2415@ic.ac.uk>
2017-02-04 14:08:28 +0000

    [DD][RJ] Added findBomb activity, transition to this after pressing play button.

---
+++ ManhattanPi-ject/app/src/main/java/com/example/fermifriends/manhattanpi_ject/FindBombActivity.java
@@ -0,0 +1,13 @@
+package com.example.fermifriends.manhattanpi_ject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FindBombActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_bomb);
+    }
+}
