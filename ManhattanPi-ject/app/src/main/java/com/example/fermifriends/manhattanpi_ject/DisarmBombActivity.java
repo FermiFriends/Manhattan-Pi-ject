737681c706df6ccf78ae4749c07e7bd91a474056
rishabh <rishabh@dyn1178-166.wlan.ic.ac.uk>
2017-02-04 15:11:41 +0000

    [RJ] Added disarmbomb activity.

---
+++ ManhattanPi-ject/app/src/main/java/com/example/fermifriends/manhattanpi_ject/DisarmBombActivity.java
@@ -0,0 +1,13 @@
+package com.example.fermifriends.manhattanpi_ject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DisarmBombActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_disarm_bomb);
+    }
+}
